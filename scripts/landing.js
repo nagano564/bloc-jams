04cc1e7bf23da3a1ddc3432b1a9e01b9790d56ee
nagano564 <nagano564@gmail.com>
2017-01-16 11:56:06 -0500

    ckpt-16 done pt2

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,38 +1,13 @@
-var pointsArray = document.getElementsByClassName("point");
-
-//var animatePoints = function(points) {
-//    var revealFirstPoint = function() {
-//        points[0].style.opacity = 1;
-//        points[0].style.transform = "scaleX(1) translateY(0)";
-//        points[0].style.msTransform = "scaleX(1) translateY(0)";
-//        points[0].style.webkitTransform = "scaleX(1) translateY(0)";
-//    }; 
-//
-//    var revealSecondPoint = function() {
-//        points[1].style.opacity = 1;
-//        points[1].style.transform = "scaleX(1) translateY(0)";
-//        points[1].style.msTransform = "scaleX(1) translateY(0)";
-//        points[1].style.webkitTransform = "scaleX(1) translateY(0)";
-//    };
-//
-//    var revealThirdPoint = function() {
-//        points[2].style.opacity = 1;
-//        points[2].style.transform = "scaleX(1) translateY(0)";
-//        points[2].style.msTransform = "scaleX(1) translateY(0)";
-//        points[2].style.webkitTransform = "scaleX(1) translateY(0)";
-//    };
-//
-//    revealFirstPoint();
-//    revealSecondPoint();
-//    revealThirdPoint();
-//};
-
-var revealPoint = function(idx) {
-    pointsArray[idx].style.opacity = 1;
-    pointsArray[idx].style.transform = "scaleX(1) translateY(0)";
-    pointsArray[idx].style.msTransform = "scaleX(1) translateY(0)";
-    pointsArray[idx].style.webkitTransform = "scaleX(1) translateY(0)";
-}
+var animatePoints = function(){
+    var revealPoint = function(){
+        
+      $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+};
+    $.each($(".point"),revealPoint);
+};
 
 var loopAndShow = function() {
     
@@ -42,21 +17,19 @@ var loopAndShow = function() {
     
 };
 
-window.onload = function(){
-
-    if (window.innerHeight > 950){
+$(window).load(function(){
+    
+    if ($(window).height > 950){
         //animatePoints(pointsArray);
         loopAndShow();
     }
     
-    var sellingPoints = document.getElementsByClassName("selling-points")[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top -  window.innerHeight + 200;
+    var scrollDistance = $(".selling-points").offset().top - $(window).height() + 200
     
-    window.addEventListener('scroll', function(event){   
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
-            //animatePoints(pointsArray);
+   $(window).scroll(function(event){
+        if ($(window).scrollTop() >= scrollDistance){
             loopAndShow();
         }
     });                         
-};
+});
               
